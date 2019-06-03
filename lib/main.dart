af78b2c4cbc4eb9118af578e7d37208084c311de
emil <loremipsum@szarasowa.ovh>
2019-06-03 13:00:22 +0200

    Start project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_web/material.dart';
+import 'package:flutter_web/foundation.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
